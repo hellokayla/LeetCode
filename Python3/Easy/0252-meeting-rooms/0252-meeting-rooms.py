c5fe105fc121c0de486a5d5132c3182a1f6f8214
Kayla <kaylacniu@gmail.com>
2026-04-11 23:44:23 -0700

    Time: 3 ms (73.41%), Space: 21.5 MB (9.72%) - LeetHub

---
+++ Python3/Easy/0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        # def(f(x)): return x[0]
+        intervals.sort(key = lambda x : x[0])
+        
+        for i in range(1, len(intervals)):
+            end_time_curr = intervals[i-1][1] # end time of curr interval
+            start_time_next = intervals[i][0] # start time of next interval
+
+            if end_time_curr > start_time_next:
+                return False
+        return True
+        
