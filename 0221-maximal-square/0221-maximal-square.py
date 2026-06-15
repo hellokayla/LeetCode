5fe1e59b45071cb8edcf2a68aef43fc8f2ac4141
Kayla <kaylacniu@gmail.com>
2026-06-15 10:43:42 -0700

    108 ms (32.75%) 33.2 MB (47.02%) 06-15-2026 0221-maximal-square 06-15-2026

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -3,20 +3,20 @@ class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         if not matrix:
             return
-
+    
         r = len(matrix) # rows  4
         c = len(matrix[0]) # columns 5
-        cache = deepcopy(matrix)
+        cache = [[int(val) for val in row] for row in matrix]
         max_res = 0
         # bottom up solution
         for i in range(0, r):
             for j in range(0, c):
                 if (i == 0 or j == 0): # border 
                     pass
-                elif (int(matrix[i][j]) > 0):
-                    cache[i][j] = 1 + min(int(cache[i][j-1]), int(cache[i-1][j]), int(cache[i-1][j-1]))
-                if int(cache[i][j]) > int(max_res):
-                    max_res = int(cache[i][j])
+                elif int(matrix[i][j]) > 0:
+                    cache[i][j] = 1 + min(cache[i][j-1], cache[i-1][j], cache[i-1][j-1])
+                if cache[i][j] > max_res:
+                    max_res = cache[i][j]
         return max_res*max_res      
 
 
