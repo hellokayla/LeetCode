2abd3ac4e8635206ec17d30d81d3ed935dace626
Kayla <kaylacniu@gmail.com>
2026-06-23 22:08:52 -0700

    66 ms (91.47%) 65.7 MB (99.88%) 06-23-2026 0981-time-based-key-value-store 06-23-2026

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,25 +1,32 @@
+import bisect
 class TimeMap:
 
     def __init__(self):
-        self.store = defaultdict(list)
+        self.times = defaultdict(list)
+        self.values = defaultdict(list)
 
+    # the key intuition is the "bar" value is at the same index as the timestamp.
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.store[key].append((timestamp, value)) # key: "foo" value: ("bar", 1)
+        self.times[key].append(timestamp)
+        self.values[key].append(value)
 
     def get(self, key: str, timestamp: int) -> str:
-        value = self.store.get(key, []) #should get out the value
-        #print("value:", value)
-        left = 0
-        right = len(value)-1
-        res = ''
-        while left <= right: 
-            mid = left + (right - left) // 2
-            if value[mid][0] <= timestamp:
-                #print("value[mid][0]:",value[mid][0])
-                left = mid + 1 
-                res = value[mid][1]
-            else:
-                right = mid - 1 
+        timestamps = self.times[key]
+        # replaces:
+        # left = 0
+        # right = len(value)-1
+        # res = ''
+        # while left <= right: 
+        #     mid = left + (right - left) // 2
+        #     if value[mid][0] <= timestamp:
+        #         left = mid + 1 
+        #         res = value[mid][1]
+        #     else:
+        #         right = mid - 1
+        idx_right  = bisect.bisect_right(timestamps, timestamp)
+        # counts how many is <= timestamp
+        # values: "foo" -> ["bar", "bar2"] timestamps: "foo"-> [1, 4]
+        res = self.values[key][idx_right-1] if idx_right else ""
         return res
 
 
@@ -28,7 +35,3 @@ class TimeMap:
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
 
-'''
-p0 - we want to create a way to store this
-
-'''
