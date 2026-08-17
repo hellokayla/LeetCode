415c90d9e92f478c647e7dc3f11c1c9b2bd7c64c
Kayla <kaylacniu@gmail.com>
2026-08-16 22:05:21 -0700

    25 ms (71.78%) 19.4 MB (13.39%) 08-16-2026 2652-sum-multiples 08-16-2026

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -1,8 +1,7 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
-        nums = list(range(1,n+1))
         res_sum = 0
-        for num in nums:
+        for num in range(1, n+1):
             if (num%3 == 0 or num%5 == 0 or num%7 == 0):
                 res_sum += num
         return res_sum
